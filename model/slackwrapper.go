d0359f847e3905e376869db1365d9409e49b31b4
Mihyaeru <mihyaeru21@gmail.com>
2016-02-08 22:37:49 +0900

    add channels

--- model/slackwrapper.go
+++ model/slackwrapper.go
@@ -18,21 +18,27 @@ type Message struct {
 }
 
 type SlackWrapper struct {
-	token  string
-	api    *slack.Client
-	team   string
-	teamID string
+	token    string
+	api      *slack.Client
+	team     string
+	teamID   string
+	channels map[string]string
 }
 
 func NewSlack(token string) *SlackWrapper {
 	return &SlackWrapper{
-		token: token,
-		api:   slack.New(token),
+		token:    token,
+		api:      slack.New(token),
+		channels: map[string]string{},
 	}
 }
 
 func (self *SlackWrapper) Start(messages chan Message) {
 	if err := self.combineTeam(); err != nil {
+		fmt.Printf("Authentication failed. token: %s\n", self.token)
+		os.Exit(1)
+	}
+	if err := self.combineChannel(); err != nil {
 		fmt.Printf("Authentication failed. token: %s\n", self.token)
 		os.Exit(1)
 	}
@@ -54,10 +60,16 @@ func (self *SlackWrapper) Start(messages chan Message) {
 }
 
 func (self *SlackWrapper) createMessage(event *slack.MessageEvent) Message {
+	channelName, ok := self.channels[event.Channel]
+	if !ok {
+		channelName = event.Channel
+	}
+
 	return Message{
 		Timestamp: event.Timestamp,
 		Team:      self.team,
 		TeamID:    self.teamID,
+		Channel:   channelName,
 		ChannelID: event.Channel,
 		UserID:    event.User,
 		Text:      event.Text,
@@ -71,5 +83,36 @@ func (self *SlackWrapper) combineTeam() error {
 	}
 	self.team = auth.Team
 	self.teamID = auth.TeamID
+	return nil
+}
+
+func (self *SlackWrapper) combineChannel() error {
+	channels, err := self.api.GetChannels(false)
+	if err != nil {
+		return err
+	}
+	for i := 0; i < len(channels); i++ {
+		channel := channels[i]
+		self.channels[channel.ID] = channel.Name
+	}
+
+	groups, err := self.api.GetGroups(false)
+	if err != nil {
+		return err
+	}
+	for i := 0; i < len(groups); i++ {
+		group := groups[i]
+		self.channels[group.ID] = group.Name
+	}
+
+	ims, err := self.api.GetIMChannels()
+	if err != nil {
+		return err
+	}
+	for i := 0; i < len(ims); i++ {
+		im := ims[i]
+		self.channels[im.ID] = im.User
+	}
+
 	return nil
 }
